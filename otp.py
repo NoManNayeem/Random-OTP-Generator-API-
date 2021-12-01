3a9aa9350ec2f98c36c19907c030aa43df1cdddc
Nayeem Islam <60138887+NoManNayeem@users.noreply.github.com>
2021-12-01 14:08:57 +0600

    Create otp.py

---
+++ otp.py
@@ -0,0 +1,8 @@
+import random
+
+def generateOTP(otp_size = 6):
+    final_otp = ''
+    for i in range(otp_size):
+        final_otp = final_otp + str(random.randint(0,9))
+    return final_otp
+print(final_otp)
